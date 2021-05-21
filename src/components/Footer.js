13af0c9b568f5d014cc6adfd5b7874400ede9aff
Lucas Lima <limalucas454@gmail.com>
2021-05-21 20:22:13 +0000

    fixing links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
     return (
         <footer className="page-footer">
             <div className="container">
-                <div className="links"><a href="cv.html">Sobre mim</a><a href="#">Fale comigo</a><a href="#">Projetos</a></div>
-                <div className="social-icons"><a href="#"><i className="icon ion-social-facebook" /></a><a href="#"><i className="icon ion-social-instagram-outline" /></a><a href="#"><i className="icon ion-social-linkedin" /></a></div>
+                <div className="links"><a href="cv">Sobre mim</a><a href="contact">Fale comigo</a><a href="projects">Projetos</a></div>
+                <div className="social-icons"><a href="https://www.facebook.com/lucas.lima.562329"><i className="icon ion-social-facebook" /></a><a href="https://www.instagram.com/llxd11"><i className="icon ion-social-instagram-outline" /></a><a href="https://www.linkedin.com/in/lucas-lima-do-nascimento-a8819018a/"><i className="icon ion-social-linkedin" /></a></div>
             </div>
         </footer>
 
--- src/components/Projeto.js
+++ src/components/Projeto.js
@@ -6,7 +6,7 @@ const Projeto = (props) => {
             <div className="project-card-no-image">
                 <h3>{props.titulo}</h3>
                 <h4>{props.desc}</h4><a className="btn btn-outline-primary btn-sm" role="button" href={props.url}>Saiba Mais</a>
-                <div className="tags"><a href="#">{props.lang}</a></div>
+                <div className="tags">{props.lang}</div>
             </div>
         </div>
     )
