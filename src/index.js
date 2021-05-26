849c29472efaf367f1bab66acc58ca2dfd078674
Lucas Lima <limalucas454@gmail.com>
2021-05-26 19:11:49 +0000

    added multiple routings

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import App from './App';
 import Projects from './Projects';
 import CV from './CV';
 import Contact from './Contact';
+import Ocinemae from './components/projects/Ocinemae';
+import Fatima from './components/projects/Fatima';
+import Braga from './components/projects/Braga';
+import Roboforge from './components/projects/Roboforge';
+import Sutive from './components/projects/Sutive';
+import Aluguel from './components/projects/Aluguel';
+import Lim from './components/projects/Lim';
+import Draftools from './components/projects/Draftools';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 
@@ -14,6 +22,14 @@ ReactDOM.render(
             <Route path="/projects" exact={true} component={Projects} />
             <Route path="/cv" exact={true} component={CV} />
             <Route path="/contact" exact={true} component={Contact} />
+            <Route path="/ocinemae" exact={true} component={Ocinemae} />
+            <Route path="/fatima" exact={true} component={Fatima} />
+            <Route path="/braga" exact={true} component={Braga} />
+            <Route path="/roboforge" exact={true} component={Roboforge} />
+            <Route path="/sutive" exact={true} component={Sutive} />
+            <Route path="/aluguel" exact={true} component={Aluguel} />
+            <Route path="/lim" exact={true} component={Lim} />
+            <Route path="/draftools" exact={true} component={Draftools} />
         </Switch>
     </ BrowserRouter>,
     document.getElementById('root')
