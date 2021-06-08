d59b87fafd1e1ce94241c107d13eef6cd20a9d39
Lucas Lima <limalucas454@gmail.com>
2021-06-08 16:11:40 +0000

    reworking project component

--- src/Projects.js
+++ src/Projects.js
@@ -15,14 +15,14 @@ const Projects = () => {
                             <h2>meus projetos</h2>
                         </div>{/* End: portfolio heading */}
                         <div className="row">
-                            <CardProjeto url={"/ocinemae"} titulo={"Podcasts - O Cinema É"} desc={"Um site que contém podcasts sobre cinema"} lang={"HTML CSS JS LARAVEL"} />
-                            <CardProjeto url={"/fatima"} titulo={"Fátima Imóveis"} desc={"Um site para uma corretora de imóveis"} lang={"HTML CSS JS laravel"} />
-                            <CardProjeto url={"/braga"} titulo={"Braga Corretor"} desc={"Um site para um corretor de imóveis"} lang={"HTML CSS JS laravel"} />
-                            <CardProjeto url={"/roboforge"} titulo={"Roboforge"} desc={"Um site para uma equipe de robótica universitária"} lang={"HTML CSS JS"} />
-                            <CardProjeto url={"/sutive"} titulo={"Sutive"} desc={"Um site para uma ideia hospitalar contra a COVID-19"} lang={"HTML CSS JS"} />
-                            <CardProjeto url={"/aluguel"} titulo={"Gerador de Recibos"} desc={"Uma aplicação que gera recibos de usuários cadastrados"} lang={"Electron JS"} />
-                            <CardProjeto url={"/lim"} titulo={"LIM Engenharia"} desc={"Um site para uma empresa de engenharia civil"} lang={"React"} />
-                            <CardProjeto url={"/draftools"} titulo={"Draftools"} desc={"Um site que faz análises de composições para League of Legends"} lang={"React"} />
+                            <CardProjeto online={1} code={"https://github.com/ArticaDev/cinema-podcasts"} url={"http://podcast.ocinemae.com.br/inicio"} titulo={"Podcasts - O Cinema É"} desc={"Um site que contém podcasts sobre cinema"} lang={"HTML CSS JS LARAVEL"} />
+                            <CardProjeto online={1} code={"https://github.com/ArticaDev/fatima-imoveis"} url={"http://fatimacorretora.com/casas"} titulo={"Fátima Imóveis"} desc={"Um site para uma corretora de imóveis"} lang={"HTML CSS JS laravel"} />
+                            <CardProjeto online={1} code={"https://github.com/ArticaDev/braga-corretor"} url={"http://bragacorretor.com/casas"} titulo={"Braga Corretor"} desc={"Um site para um corretor de imóveis"} lang={"HTML CSS JS laravel"} />
+                            <CardProjeto online={1} code={"https://github.com/roboforgeufu/roboforgeufu.github.io"} url={"https://roboforgeufu.github.io/"} titulo={"Roboforge"} desc={"Um site para uma equipe de robótica universitária"} lang={"HTML CSS JS"} />
+                            <CardProjeto online={1} code={"https://github.com/roboforgeufu/sutive"} url={"https://roboforgeufu.github.io/sutive/"} titulo={"Sutive"} desc={"Um site para uma ideia hospitalar contra a COVID-19"} lang={"HTML CSS JS"} />
+                            <CardProjeto code={"https://github.com/ArticaDev/rent-receipt"} titulo={"Gerador de Recibos"} desc={"Uma aplicação que gera recibos de usuários cadastrados"} lang={"Electron JS"} />
+                            <CardProjeto online={1} code={"https://github.com/ArticaDev/limengenharia"} url={"https://limengenharia.com/"} titulo={"LIM Engenharia"} desc={"Um site para uma empresa de engenharia civil"} lang={"React"} />
+                            <CardProjeto code={"https://github.com/LLxD/draftools"} titulo={"Draftools"} desc={"Um site que faz análises de composições para League of Legends"} lang={"React"} />
                         </div>
                     </div>
                 </section>
--- src/components/CardProjeto.js
+++ src/components/CardProjeto.js
@@ -11,15 +11,37 @@ const Projeto = (props) => {
         color = "black"
     }
 
-    return (
-        <div className="col-md-6 col-lg-4">
-            <div className="project-card-no-image" style={{ borderTop: "4px solid " + color }}>
-                <h3>{props.titulo}</h3>
-                <h4>{props.desc}</h4><a className="btn btn-outline-primary btn-sm" role="button" href={props.url}>Saiba Mais</a>
-                <div className="tags">{props.lang}</div>
-            </div>
-        </div >
-    )
+    if (props.online === 1) {
+
+        return (
+            <div className="col-md-6 col-lg-4">
+                <div className="project-card-no-image" style={{ borderTop: "4px solid " + color }}>
+                    <h3>{props.titulo}</h3>
+                    <h4>{props.desc}</h4>
+                    <div className="d-flex justify-content-around">
+                        <a className="btn btn-outline-primary btn-sm" role="button" href={props.url}>Veja online!</a>
+                        <a className="btn btn-outline-primary btn-sm" role="button" href={props.code}><i className="ion-social-github"></i></a>
+                        <div className="tags">{props.lang}</div>
+                    </div>
+                </div>
+            </div >
+        )
+    }
+    else {
+        return (
+            <div className="col-md-6 col-lg-4">
+                <div className="project-card-no-image" style={{ borderTop: "4px solid " + color }}>
+                    <h3>{props.titulo}</h3>
+                    <h4>{props.desc}</h4>
+                    <div className="d-flex justify-content-around">
+                        <a className="btn btn-outline-primary btn-sm" role="button" href={props.code}><i className="ion-social-github"></i></a>
+                        <div className="tags">{props.lang}</div>
+                    </div>
+                </div>
+            </div >
+        )
+    }
+
 }
 
 export default Projeto
